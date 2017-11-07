e1f0798877f204b064296e387688d560c2098cf1
Andrew Kerley <ajkerley@mail.lipscomb.edu>
2017-11-06 18:18:48 -0600

    Added simple data sanitization

--- DataBuilder.py
+++ DataBuilder.py
@@ -15,7 +15,16 @@ class DataBuilder:
         #           , "Dr. Gwen Pennington", "Dr. Lillian Price", "Dr. Dan Prince", "Dr. Raleigh Stewart", "Dr. Ana Syphax", "Dr. Ben Turner"
         #           , "Dr. Geoffrey Weiss", "Dr. Wesley Williams"]
         doctors = raw_input("Please enter the Doctor's name: ")
-        deaNumber = raw_input("Please enter the DEA regulation number: ")
+
+        while True:
+            try:
+                deaNumber = raw_input("Please enter the DEA regulation number: ")
+                val = int(deaNumber)
+                break
+            except:
+                print("Please enter a valid number.")
+
+
         address = raw_input("Please enter the patient's address: ")
         birth = raw_input("Please enter the patient's date of birth: ")
 
@@ -26,7 +35,13 @@ class DataBuilder:
         # doctor_phone_numbers = ["202-555-0102", "202-555-0171", "202-555-0193", "202-555-0111", "202-555-0191", "202-555-0147"
         #                        , "202 - 555 - 0114", "202 - 555 - 0180" ,"202 - 555 - 0127", "202 - 555 - 0180" ,"202 - 555 - 0128"
         #                        , "202 - 555 - 0107"]
-        doctor_phone_numbers = raw_input("Please enter the Doctor's phone number: ")
+        while True:
+            try:
+                doctor_phone_numbers = raw_input("Please enter the Doctor's phone number: ")
+                val = int(doctor_phone_numbers)
+                break
+            except:
+                print("Please enter a valid phone number.")
 
         # patient_names = ["Mario Speedwagon", "Petey Cruiser", "Anna Sthesia", "Paul Molive", "Anna Mull", "Gail Forcewind"
         #                 ,"Paige Turner", "Bob Frapples", "Walter Melon", "Nick R.Bocker", "Barb Ackue", "Buck Kinnear"
@@ -38,10 +53,35 @@ class DataBuilder:
         #               "Furosemide","Metoprolol"]
         drug_names = raw_input("Please enter the prescription drug name: ")
         # drug_amounts = ["500mg", "200mg", "600mg", "100mg", "50mg", "900mg"]
-        drug_amounts = raw_input("Please enter the drug amount in milligrams: ")
-        tablet_amounts = raw_input("Please enter the tablet amount: ")
+
+        while True:
+            try:
+                drug_amounts = raw_input("Please enter the drug amount in milligrams: ")
+                val = int(drug_amounts)
+                break
+            except:
+                print("Please enter a valid number.")
+
+
+
+        while True:
+            try:
+                tablet_amounts = raw_input("Please enter the tablet amount: ")
+                val = int(tablet_amounts)
+                break
+            except:
+                print("Please enter a valid number.")
+
         patient_instructions = raw_input("Please enter the patient instructions: ")
-        refills = raw_input("Please enter the number of refills: ")
+
+        while True:
+            try:
+                refills = raw_input("Please enter the number of refills: ")
+                val = int(refills)
+                break
+            except:
+                print("Please enter a valid number.")
+
 
         block_data = {'doctor' : doctors,
                       'DEA number' : deaNumber,
